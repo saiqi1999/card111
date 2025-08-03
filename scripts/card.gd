cfe2d8fed305aaf10d808a10f8b63b6ed532b0d6
alapaka <90421945+saiqi1999@users.noreply.github.com>
2025-08-03 21:28:47 +0800

    修改card.gd：确保所有卡牌都打印纹理和坐标信息，即使没有特定图片

--- scripts/card.gd
+++ scripts/card.gd
@@ -31,26 +31,33 @@ func _ready() -> void:
 		cost_label.text = str(cost)
 	if description_label:
 		description_label.text = description
+	# 设置卡牌图片纹理
 	if card_sprite and card_image:
 		card_sprite.texture = card_image
-		# 打印卡牌图片信息
-		print("卡牌 '%s' 图片已加载: %s" % [card_name, card_image.resource_path if card_image.resource_path else "内存中的图片"])
-		print("卡牌 '%s' 图片位置: (%d, %d)" % [card_name, global_position.x, global_position.y])
-		print("卡牌 '%s' 图片大小: 宽度 %d, 高度 %d" % [card_name, card_image.get_width(), card_image.get_height()])
-		
-		# 计算并打印卡牌四个顶点的坐标
-		var width = card_sprite.size.x
-		var height = card_sprite.size.y
-		var top_left = global_position + card_sprite.position - Vector2(width/2, height/2)
-		var top_right = global_position + card_sprite.position + Vector2(width/2, -height/2)
-		var bottom_left = global_position + card_sprite.position + Vector2(-width/2, height/2)
-		var bottom_right = global_position + card_sprite.position + Vector2(width/2, height/2)
-		
-		print("卡牌 '%s' 四个顶点坐标:" % card_name)
-		print("左上角: (%d, %d)" % [top_left.x, top_left.y])
-		print("右上角: (%d, %d)" % [top_right.x, top_right.y])
-		print("左下角: (%d, %d)" % [bottom_left.x, bottom_left.y])
-		print("右下角: (%d, %d)" % [bottom_right.x, bottom_right.y])
+	
+	# 打印卡牌图片信息 - 无论是否有特定图片都打印
+	if card_sprite:
+		var texture = card_sprite.texture
+		if texture:
+			print("卡牌 '%s' 图片已加载: %s" % [card_name, texture.resource_path if texture.resource_path else "内存中的图片"])
+			print("卡牌 '%s' 图片位置: (%d, %d)" % [card_name, global_position.x, global_position.y])
+			print("卡牌 '%s' 图片大小: 宽度 %d, 高度 %d" % [card_name, texture.get_width(), texture.get_height()])
+			
+			# 计算并打印卡牌四个顶点的坐标
+			var width = card_sprite.size.x
+			var height = card_sprite.size.y
+			var top_left = global_position + card_sprite.position - Vector2(width/2, height/2)
+			var top_right = global_position + card_sprite.position + Vector2(width/2, -height/2)
+			var bottom_left = global_position + card_sprite.position + Vector2(-width/2, height/2)
+			var bottom_right = global_position + card_sprite.position + Vector2(width/2, height/2)
+			
+			print("卡牌 '%s' 四个顶点坐标:" % card_name)
+			print("左上角: (%d, %d)" % [top_left.x, top_left.y])
+			print("右上角: (%d, %d)" % [top_right.x, top_right.y])
+			print("左下角: (%d, %d)" % [bottom_left.x, bottom_left.y])
+			print("右下角: (%d, %d)" % [bottom_right.x, bottom_right.y])
+		else:
+			print("卡牌 '%s' 没有纹理" % card_name)
 	
 	# 连接输入事件
 	set_process_input(true)
